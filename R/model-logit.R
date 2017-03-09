b663ce82f49e23d9fc622435e3bba480fa565ba6
christophergandrud <christopher.gandrud@gmail.com>
2017-03-09 13:41:13 +0100

    networkD3 models and logit ref

--- R/model-logit.R
+++ R/model-logit.R
@@ -1,6 +1,73 @@
 #' Logistic Regression for Dichotomous Dependent Variables
 #'
+#' @param formula an object of class "formula" (or one that can be coerced to
+#'   that class): a symbolic description of the model to be fitted.
+#' @param data an optional data frame, list or environment (or object coercible
+#'   by `as.data.frame` to a data frame) containing the variables in the model.
+#'   If not found in data, the variables are taken from `environment(formula)`,
+#'   typically the environment from which `lm`` is called.
+#'
+#'   Imputed data sets created with \link{amelia} or \code{\link{to_zelig_mi}}
+#'   can also be supplied.
+#' @param ... arguments to pass to \code{\link{glm}}.
+#' @param model a character string specifying the model type. NOTE: only
+#'   required if using the Zelig 4 wrappers.
+#' @param weights an optional vector of weight values or character string
+#'   identifying a weighting variable in `data`. Weights adjust the observed
+#'   sample distribution in the data to an underlying population of interest.
+#'
+#'   - If the supplied weights are all integer values, then `zelig` rebuilds a new
+#'   version of the dataset by duplicating observations according to their
+#'   weight (and removing observations with zero weight).
+#'
+#'   - If the weights are continuously valued, `zelig`` bootstraps the supplied
+#'   dataset, using the relative weights as bootstrap probabilities.
+#'
+#' @param by an optional character string identifying a variable in `data`
+#'   to conduct the model analysis along. This can be used to run separate
+#'   models for different levels of a categorical variable.
+#' @param bootstrap logical whether or not to bootstrap the data set.
+#' @param cite logical whether or not to return model citation information to
+#'   the console when the model has been estimated.
+#'
+#' @details
+#' Logistic regression specifies a dichotomous dependent variable as a function
+#' of a set of explanatory variables.
+#'
 #' Vignette: \url{http://docs.zeligproject.org/en/latest/zelig-logit.html}
+#'
+#' @examples
+#' \dontrun{
+#' # Workflow: Zelig 5 Reference Classes
+#' z5 <- zlogit$new()
+#' z5$zelig(Y ~ X1 + X ~ X, weights = w, data = mydata)
+#' z5$setx()
+#' z5$sim()
+#'
+#' # Workflow: Zelig 4 Wrappers
+#' z.out <- zelig(Y ~ X1 + X2, model = "logit", weights = w, data = mydata)
+#' x.out <- setx(z.out)
+#' s.out <- sim(z.out, x = x.out, x1 = NULL)
+#' }
+#'
+#' # Basic example with Zelig 4 wrappers
+#' data(turnout)
+#'
+#' # Estimate model
+#' z.out1 <- zelig(vote ~ age + race, model = "logit", data = turnout,
+#'                 cite = FALSE)
+#'
+#' # Set fitted values for a 36 year old white subject
+#' x.out1 <- setx(z.out1, age = 36, race = "white")
+#'
+#' # Simulate
+#' s.out1 <- sim(z.out1, x = x.out1)
+#'
+#' \dontrun{
+#' # Plot quantities of interest
+#' plot(s.out1)
+#' }
+#'
 #' @import methods
 #' @export Zelig-logit
 #' @exportClass Zelig-logit
--- R/model-ls.R
+++ R/model-ls.R
@@ -1,40 +1,40 @@
 #' Least Squares Regression for Continuous Dependent Variables
-#' 
-#' @param formula an object of class "formula" (or one that can be coerced to 
-#'   that class): a symbolic description of the model to be fitted. 
-#' @param data an optional data frame, list or environment (or object coercible 
-#'   by `as.data.frame` to a data frame) containing the variables in the model. 
-#'   If not found in data, the variables are taken from `environment(formula)`, 
-#'   typically the environment from which `lm`` is called. 
-#'   
-#'   Imputed data sets created with \link{amelia} or \code{\link{to_zelig_mi}} 
+#'
+#' @param formula an object of class "formula" (or one that can be coerced to
+#'   that class): a symbolic description of the model to be fitted.
+#' @param data an optional data frame, list or environment (or object coercible
+#'   by `as.data.frame` to a data frame) containing the variables in the model.
+#'   If not found in data, the variables are taken from `environment(formula)`,
+#'   typically the environment from which `lm`` is called.
+#'
+#'   Imputed data sets created with \link{amelia} or \code{\link{to_zelig_mi}}
 #'   can also be supplied.
 #' @param ... arguments to pass to \code{\link{lm}}.
-#' @param model a character string specifying the model type. NOTE: only 
+#' @param model a character string specifying the model type. NOTE: only
 #'   required if using the Zelig 4 wrappers.
-#' @param weights an optional vector of weight values or character string 
-#'   identifying a weighting variable in `data`. Weights adjust the observed 
-#'   sample distribution in the data to an underlying population of interest. 
-#'   
-#'   - If the supplied weights are all integer values, then `zelig` rebuilds a new 
-#'   version of the dataset by duplicating observations according to their 
+#' @param weights an optional vector of weight values or character string
+#'   identifying a weighting variable in `data`. Weights adjust the observed
+#'   sample distribution in the data to an underlying population of interest.
+#'
+#'   - If the supplied weights are all integer values, then `zelig` rebuilds a new
+#'   version of the dataset by duplicating observations according to their
 #'   weight (and removing observations with zero weight).
-#'   
-#'   - If the weights are continuously valued, `zelig`` bootstraps the supplied 
+#'
+#'   - If the weights are continuously valued, `zelig`` bootstraps the supplied
 #'   dataset, using the relative weights as bootstrap probabilities.
-#'   
-#' @param by an optional character string identifying a variable in `data` 
-#'   to conduct the model analysis along. This can be used to run separate 
+#'
+#' @param by an optional character string identifying a variable in `data`
+#'   to conduct the model analysis along. This can be used to run separate
 #'   models for different levels of a categorical variable.
 #' @param bootstrap logical whether or not to bootstrap the data set.
-#' @param cite logical whether or not to return model citation information to 
-#'   the console when the model has been estimated. 
+#' @param cite logical whether or not to return model citation information to
+#'   the console when the model has been estimated.
 #'
-#' @details Use least squares regression analysis to estimate the best linear 
+#' @details Use least squares regression analysis to estimate the best linear
 #'   predictor for the specified dependent variables.
-#' 
+#'
 #' Vignette: [http://docs.zeligproject.org/en/latest/zelig-ls.html](http://docs.zeligproject.org/en/latest/zelig-ls.html)
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' # Workflow: Zelig 5 Reference Classes
@@ -43,35 +43,36 @@
 #' z5$setx()
 #' z5$sim()
 #' z5$graph()
-#' 
+#'
 #' # Workflow: Zelig 4 Wrappers
-#' z.out <- zelig(Y ~ X1 + X2, model = "ls", weights = w, data = mydata)
+#' z.out <- zelig(Y ~ X1 + X2, model = "ls", weights = w, data = mydata,
+#'                cite = FALSE)
 #' x.out <- setx(z.out)
 #' s.out <- sim(z.out, x = x.out)
 #' plot(s.out)
 #' }
-#' 
-#' 
+#'
+#'
 #' # Basic Example with First Differences (using Zelig 4 wrappers) #
-#' 
+#'
 #' data(macro)
-#' 
+#'
 #' # Estimate model
 #' z.out1 <- zelig(unem ~ gdp + capmob + trade, model = "ls", data = macro)
-#' 
-#' # Set explanatory variables to their default (mean/mode) values, with high 
+#'
+#' # Set explanatory variables to their default (mean/mode) values, with high
 #' # (80th percentile) and low (20th percentile) values for the trade variable
 #' x.high <- setx(z.out1, trade = quantile(macro$trade, 0.8))
 #' x.low <- setx(z.out1, trade = quantile(macro$trade, 0.2))
-#' 
+#'
 #' # Simulate quantities of interest
 #' s.out1 <- sim(z.out1, x = x.high, x1 = x.low)
-#' 
+#'
 #' \dontrun{
 #' # Plot first differences
 #' plot(s.out1)
 #' }
-#' 
+#'
 #' @import methods
 #' @exportClass Zelig-ls
 #'
@@ -102,7 +103,7 @@ zls$methods(
     .self$model.call <- .self$zelig.call
     callSuper(formula = formula, data = data, ...,
               weights = weights, by = by, bootstrap = bootstrap)
-    
+
     # Automated Background Test Statistics and Criteria
     rse <- lapply(.self$zelig.out$z.out, (function(x) vcovHC(x, type = "HC0")))
     rse.se <- sqrt(diag(rse[[1]]))                 # Needs to work with "by" argument
@@ -139,24 +140,24 @@ zls$methods(
         sigma2 <- sigma
         -1/2 * (sum(log(sigma2) + (y -(X%*%beta))^2/sigma2))
     }
-    
+
     getVb<-function(Dboot){
       Dbar <- matrix(apply(Dboot,2,mean),nrow=B, ncol=length(Dhat), byrow=TRUE)
       Diff <- Dboot - Dbar
       Vb <- (t(Diff) %*% Diff) / (nrow(Dboot)-1)
       return(Vb)
     }
-    
+
     getSigma<-function(lm.obj){
       return(sum(lm.obj$residuals^2)/(nrow(model.matrix(lm.obj))-ncol(model.matrix(lm.obj))))
     }
-    
+
     D.est<-function(formula,data){
       lm1 <- lm(formula,data, y=TRUE)
       mm <- model.matrix(lm1)
       y <- lm1$y
       sigma <- getSigma(lm1)
-    
+
       grad <- apply(cbind(y,mm),1,function(x) numericGradient(ll.normal.bsIM, lm1$coefficients, y=x[1], X=x[2:length(x)], sigma=sigma))
       meat <- grad%*%t(grad)
       bread <- -solve(vcov(lm1))
@@ -169,7 +170,7 @@ zls$methods(
         mm <- model.matrix(lm1)
         y <- lm1$y
         sigma <- getSigma(lm1)
-        
+
         grad <- apply(cbind(y,mm),1,function(x) numericGradient(ll.normal.bsIM, lm1$coefficients, y=x[1], X=x[2:length(x)], sigma=sigma))
         meat <- grad%*%t(grad)
         bread <- -solve(vcov(lm1))
@@ -177,7 +178,7 @@ zls$methods(
 
         muB<-lm1$fitted.values
         DB <- matrix(NA, nrow=B2, ncol=length(Dhat))
-            
+
         for(j in 1:B2){
           yB2 <- rnorm(nrow(data), muB, sqrt(sigma))
           lm1B2 <- lm(yB2 ~ mm-1)
@@ -193,14 +194,14 @@ zls$methods(
 
         return(list(Dhat=Dhat,T=T))
     }
-    
+
     Dhat <- D.est(formula=.self$formula, data=.self$data)
     lm1 <- lm(formula=.self$formula, data=.self$data)
     mu <- lm1$fitted.values
     sigma <- getSigma(lm1)
     n <- length(mu)
     yname <- all.vars(.self$formula[[2]])
-    
+
     Dboot <- matrix(NA, nrow=B, ncol=length(Dhat))
     bootdata<-data
     for(i in 1:B){
@@ -214,7 +215,7 @@ zls$methods(
     Vb <- getVb(Dboot)
     omega <- t(Dhat) %*% solve(Vb) %*% Dhat
     pb = (B+1-sum(T< as.numeric(omega)))/(B+1)
-    
+
     .self$test.statistics$gim <- list(stat=omega, pval=pb)
 
     # When method used, add to references
